62f2430de521e9da85b08b13b23b058c19ef1b4b
Stephanie Rimel <srimel1@student.gsu.edu>
2020-12-05 13:34:33 -0500

    Pantry to grocery list dialog with delete option
    //TODO add recipe dialog

--- app/src/main/java/com/my/moms/pantry/scraps/DonationAdapter.java
+++ app/src/main/java/com/my/moms/pantry/scraps/DonationAdapter.java
@@ -1,44 +1,44 @@
-package com.my.moms.pantry;
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
-
-import java.util.List;
-
-public class DonationAdapter extends ArrayAdapter<com.my.moms.pantry.DonationOption> {
-
-    public DonationAdapter(Context context, List<com.my.moms.pantry.DonationOption> donationOptions) {
-        super(context, 0, donationOptions);
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder vh;
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.pantry_dialog, parent, false);
-            vh = new ViewHolder(convertView);
-            convertView.setTag(vh);
-        } else vh = (ViewHolder) convertView.getTag();
-
-        com.my.moms.pantry.DonationOption option = getItem(position);
-
-        vh.description.setText(option.description);
-        vh.amount.setText(option.amount);
-
-        return convertView;
-    }
-
-    private static final class ViewHolder {
-        TextView description;
-        TextView amount;
-
-        public ViewHolder(View v) {
-            description = (TextView) v.findViewById(R.id.item_name);
-            amount = (TextView) v.findViewById(R.id.item_quantity);
-        }
-    }
-}
+//package com.my.moms.pantry;
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ArrayAdapter;
+//import android.widget.TextView;
+//
+//import java.util.List;
+//
+//public class DonationAdapter extends ArrayAdapter<com.my.moms.pantry.DonationOption> {
+//
+//    public DonationAdapter(Context context, List<com.my.moms.pantry.DonationOption> donationOptions) {
+//        super(context, 0, donationOptions);
+//    }
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        ViewHolder vh;
+//        if (convertView == null) {
+//            convertView = LayoutInflater.from(getContext()).inflate(R.layout.pantry_dialog, parent, false);
+//            vh = new ViewHolder(convertView);
+//            convertView.setTag(vh);
+//        } else vh = (ViewHolder) convertView.getTag();
+//
+//        com.my.moms.pantry.DonationOption option = getItem(position);
+//
+//        vh.description.setText(option.description);
+//        vh.amount.setText(option.amount);
+//
+//        return convertView;
+//    }
+//
+//    private static final class ViewHolder {
+//        TextView description;
+//        TextView amount;
+//
+//        public ViewHolder(View v) {
+//            description = (TextView) v.findViewById(R.id.item_name);
+//            amount = (TextView) v.findViewById(R.id.item_quantity);
+//        }
+//    }
+//}
